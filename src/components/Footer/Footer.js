47349193f8479f4f790c246c6da4ff96a12e5255
Noorul Ashkar Haja Najumudeen <noorulashkar.n@ad.infosys.com>
2023-09-26 10:14:48 +0530

    second

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,16 +7,16 @@ const Footer = () => {
       <hr style={{width:'90%', marginTop:20}}/>
       <span className="name">
         Made by {" "}
-        <a href="https://www.youtube.com/channel/UCWFSKtpi58Il_FuiF7kqcgA" target='_blank'>ashkar_tech</a>
+        <a href="https://www.youtube.com/channel/UCWFSKtpi58Il_FuiF7kqcgA" target='_blank' rel="noreferrer">ashkar_tech</a>
       </span>
       <div className="iconContainer">
-        <a href="https://github.com/Ashkar-Noorul" target='_blank'>
+        <a href="https://github.com/Ashkar-Noorul" target='_blank' rel="noreferrer">
           <i className="fab fa-github fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/noorul-ashkar-2bb1b71a4/" target='_blank'>
+        <a href="https://www.linkedin.com/in/noorul-ashkar-2bb1b71a4/" target='_blank' rel="noreferrer">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://www.youtube.com/channel/UCWFSKtpi58Il_FuiF7kqcgA" target='_blank'>
+        <a href="https://www.youtube.com/channel/UCWFSKtpi58Il_FuiF7kqcgA" target='_blank' rel="noreferrer">
           <i className="fab fa-youtube fa-2x"></i>
         </a>
       </div>
